c22ab787bd6e0f76bf95632f5e93763e82ad71bc
SirrorsMoore1975 <chadwickau@hotmail.com>
2026-04-30 02:29:12 +0800

    create useWindowSize to calculate the windows size when called

---
+++ my-analog-clock/src/utilities/WindowSize.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+  const [ size, setSize ] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  useEffect(()=> {
+    const handleSize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+      window.addEventListener('resize', handleSize);
+      return () => { window.removeEventListener('resize', handleSize); }
+    }
+  }, []);
+  return size;
+};
+
+export default useWindowSize;
