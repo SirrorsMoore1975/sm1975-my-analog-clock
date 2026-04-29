9eb7232e71d3ee0927dfbd5c686043bc5dfc540b
chadwick <chadwickau@hotmail.com>
2026-04-29 17:34:37 +0800

    feat: display mouse position in number

--- my-analog-clock/src/App.js
+++ my-analog-clock/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import useMousePosition from "./utilities/MousePosition.js";
 
 function App() {
+  const { x, y } = useMousePosition();
+
   return (
     <div className="App">
-      <header className=""></header>
+      <header className="">Mouse Position Testing</header>
+
+      <div>
+        Mouse at: ({x}, {y})
+      </div>
     </div>
   );
 }
