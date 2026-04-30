63ef4decb1a69fad9d4cf9d4a179b0f8c1fccce8
SirrorsMoore1975 <chadwickau@hotmail.com>
2026-05-01 01:38:38 +0800

    WIP: HelloWorld.jsx

---
+++ my-analog-clock/src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+/* For Debugging Purposes */
+
+const HelloWorld = () => {
+  const [helloworld, sethelloworld] = useState("")
+  return (
+    <>
+    
+    </>
+  );
+};
+
