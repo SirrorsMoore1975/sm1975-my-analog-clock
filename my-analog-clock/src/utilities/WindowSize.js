f50e8a912f6d10efd357e4029b968dd32b3438f1
SirrorsMoore1975 <chadwickau@hotmail.com>
2026-05-04 01:12:21 +0800

    fix WindowSize not showing at initial

--- my-analog-clock/src/utilities/WindowSize.js
+++ my-analog-clock/src/utilities/WindowSize.js
@@ -1,7 +1,12 @@
 import { useState, useEffect } from 'react';
 
 const useWindowSize = () => {
-  const [ windowSize, setWindowSize ] = useState({ width: undefined, height: undefined });
+  const [ windowSize, setWindowSize ] = useState(()=>{
+    if (typeof window !== 'undefined'){
+      return {width: window.innerWidth, height: window.innerHeight}
+    }
+    return {width: 0, height:0}
+  });
   useEffect(() => {
     const handleWindowSize = () => {
       setWindowSize(
