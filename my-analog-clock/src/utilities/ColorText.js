36d9220636928b2b00d0fa485e86006d43a6b1cc
chadwick <chadwickau@hotmail.com>
2026-05-07 17:14:37 +0800

    WIP: color not used
    possible style color is an Object not Array

--- my-analog-clock/src/utilities/ColorText.js
+++ my-analog-clock/src/utilities/ColorText.js
@@ -1,35 +1,49 @@
 import React, { useState, useEffect } from "react";
+const defaultColorScheme = {
+  red: "rgb(255,0,0)",
+  brown: "rgb(255,255,0)",
+  green: "rgb(0,255,0)",
+  yellow: "rgb(0,255,255)",
+  blue: "rgb(0,0,255)",
+  pink: "rgb(255,0,255)",
+};
 
-const ColorText = ({ message, colorScheme }) => {
-  const [ colors, setColors ] = useState({});
+const ColorText = ({ message, colorScheme = defaultColorScheme }) => {
+  //const [ colors, setColors ] = useState({});
   //const [colorIdx, setColorIdx] = useState(0);
-  const colorKeys = Object.keys(colors);
+  const colorKeys = Object.keys(colorScheme);
+  const colorLength = Object.entries(colorScheme).length;
+  const colorValues = Object.values(colorScheme);
+  const [offset, setOffset] = useState(0);
   //const colorSpan = message.length / colorBreakPoint;
   //let currentColor = 0;
 
-  useEffect(()=>{
-    const intervalId = setInterval(()=>{
-      setColors(()=>{
-        if (colorScheme && !Array.isArray(colorScheme) && typeof colorScheme === 'object'){
-          return colorScheme;
-        } else {
-          return { 'red':'rgb(255,0,0)', 'brown':'rgb(255,255,0)', 'green':'rgb(0,255,0)', 'yellow':'rgb(0,255,255)', 'blue':'rgb(0,0,255)', 'pink':'rgb(255,0,255)'};
-        }
-
-      });
+  useEffect(() => {
+    const intervalId = setInterval(() => {
       //setColorIdx((prevIndex)=>(prevIndex + 1) % colors.length);
-    }, 250);
-    return () => { clearInterval(intervalId); }
-  },[colors, colorScheme])
-  
+      setOffset((prev) => (prev + 1) % colorLength);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [colorLength]);
+
   return (
-  <>
-    <span style={{ fontSize: 24, color: colors[colorKeys] }}>
-      {`${message}`}
-    </span>
-  
-  </>
-  )
+    <>
+      <span>
+        {message.split("").map((char, idx) => (
+          <span
+            key={idx}
+            style={{
+              fontSize: 24,
+              color: colorValues[offset + idx] % colorLength,
+            }}
+          >
+            {`${char} `}
+          </span>
+        ))}
+      </span>
+    </>
+  );
 };
 export default ColorText;
-
