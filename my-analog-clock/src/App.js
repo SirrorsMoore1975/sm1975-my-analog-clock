880aa65ddae5a3505b1abd82ab3001da2b6993b3
chadwick <chadwickau@hotmail.com>
2026-05-08 17:18:48 +0800

    WIP:clock face is out of boundary for y-axis

--- my-analog-clock/src/App.js
+++ my-analog-clock/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 
 import HelloWorld from "./components/HelloWorld.jsx";
+import ClockFace from "./components/ClockFace.jsx";
 
 function App() {
-  const DebugMode = true;
+  const DebugMode = false;
 
   return (
     <div className="App">
       <div>{DebugMode ? <HelloWorld /> : ""}</div>
+      <ClockFace />
     </div>
   );
 }
