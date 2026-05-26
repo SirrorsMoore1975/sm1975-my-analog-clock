791969df3badb3a73e2ec0928f406871a00bc895
chadwick <chadwickau@hotmail.com>
2026-05-26 18:16:27 +0800

    WIP: share common valuables for mouse position & screen size

--- my-analog-clock/src/components/AnalogClock.jsx
+++ my-analog-clock/src/components/AnalogClock.jsx
@@ -1,1 +1,16 @@
 import { useState, useEffect, useRef } from "react";
+import useMousePosition from "../utilities/MousePosition.js";
+import useWindowSize from "../utilities/WindoeSize.js";
+import useCurrentTime from "../utilities/CurrentTime.js";
+import ClockFace from "./ClockFace.jsx";
+import ClockHand from "./ClockHand.jsx";
+
+const AnalogClock = () => {
+  const {x,y} = useMousePosition();
+  const {wdith, height} = useWindowSize();
+  const analogClockRef = useRef(null);
+  const [ offset, setOffset ] = useState({ x:0, y:0 });
+  useEffect(()=>{},[])
+
+  return ()
+}
