511d90a74b30bc585d842663b1c187163965b956
chadwick <chadwickau@hotmail.com>
2026-06-26 17:40:07 +0800

    init centralizing hand and clock

---
+++ my-analog-clock/src/components/CentralContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CentralContainer({ childern }){
+  // The Central Container suppose to contain the clock and hold things under the same roof.
+  //
+  }
+  return (
+  <>
+    <div>
+      {children}
+    </div>
+  </>
+  )
+}
