c6dd456c3e5976dcc3973a7bcb1364875cf015b4
chadwick <chadwickau@hotmail.com>
2026-07-02 12:07:00 +0800

    WIP: adding nested div to AnalogClock

--- my-analog-clock/src/components/AnalogClock.jsx
+++ my-analog-clock/src/components/AnalogClock.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import useMousePosition from "../utilities/MousePosition.js";
+import useWindowSize from "../utilities/WindowSize.js";
+
 import useCurrentTime from "../utilities/CurrentTime.js";
 import ClockFace from "./ClockFace.jsx";
 import ClockHand from "./ClockHand.jsx";
+
+import "../styles/ClockFace.css";
 
 const AnalogClock = () => {
   const {
@@ -20,43 +25,81 @@ const AnalogClock = () => {
     meridiem,
   } = useCurrentTime();
 
+  const { x, y } = useMousePosition();
+  const { width, height } = useWindowSize();
+  const clockFaceRef = useRef(null);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
   const secondAngle = (seconds / 60) * 360;
   const minutesAngle = (minutes / 60) * 360 + (seconds / 60) * 6;
   const hourAngle = ((hours % 12) / 12) * 360 + (minutes / 60) * 30;
   const fullTime = `(${weeknumber}) ${weekday} ${day}${suffix} ${months} ${year} ${hh}:${mm}:${ss} ${meridiem}`;
 
+  useEffect(() => {
+    const margin = 20;
+    const rect = clockFaceRef.current.getBoundingClientRect();
+    const maxX = width - rect.width - margin;
+    const maxY = height - rect.height - margin;
+    const minX = margin;
+    const minY = margin;
+    setOffset({
+      x: Math.min(Math.max(x, minX), maxX),
+      y: Math.min(Math.max(y, minY), maxY),
+    });
+  }, [x, y, width, height]);
+
   return (
     <>
-      <div>
-        <ClockFace />
-        <ClockHand
-          className="secondsHand"
-          color="#e83151"
-          handHeight={1}
-          handsDegree={secondAngle}
-          handWidth={90}
-          zIndex={9997}
-          borderTop="2px solid #E83151"
-        />
-        <ClockHand
-          className="minutesHand"
-          color="#cccccc"
-          handHeight={1}
-          handsDegree={minutesAngle}
-          handWidth={75}
-          zIndex={9996}
-          borderTop="4px solid #CCC"
-        />
-        <ClockHand
-          className="hoursHand"
-          color="#cccccc"
-          handHeight={1}
-          handsDegree={hourAngle}
-          handWidth={65}
-          zIndex={9995}
-          borderTop="8px solid #CCC"
-        />
-        {fullTime}
+      <div
+        style={{
+          position: "fixed",
+          pointEvents: "none",
+          top: 0,
+          left: 0,
+          zIndex: 9999,
+        }}
+      >
+        <div
+          ref={clockFaceRef}
+          style={{
+            position: "absolute",
+            transform: `translate(${offset.x}px,${offset.y}px)`,
+            pointEvents: "none",
+            width: "200px",
+            height: "200px",
+            transformOrigin: "center center",
+          }}
+        >
+          <ClockFace />
+          <ClockHand
+            className="secondsHand"
+            color="#e83151"
+            handHeight={1}
+            handsDegree={secondAngle}
+            handWidth={90}
+            zIndex={9997}
+            borderTop="2px solid #E83151"
+          />
+          <ClockHand
+            className="minutesHand"
+            color="#cccccc"
+            handHeight={1}
+            handsDegree={minutesAngle}
+            handWidth={75}
+            zIndex={9996}
+            borderTop="4px solid #CCC"
+          />
+          <ClockHand
+            className="hoursHand"
+            color="#cccccc"
+            handHeight={1}
+            handsDegree={hourAngle}
+            handWidth={65}
+            zIndex={9995}
+            borderTop="8px solid #CCC"
+          />
+          {fullTime}
+        </div>
       </div>
     </>
   );
