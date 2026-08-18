6c5308081aefa90f691237ad1aff1bbc9ae4075b
chadwick <chadwickau@hotmail.com>
2026-08-18 14:32:06 +0800

    change clock rotate msg to 1000ms
    glitch: sec hands and rotate msg not align

--- my-analog-clock/src/components/AnalogClock.jsx
+++ my-analog-clock/src/components/AnalogClock.jsx
@@ -33,7 +33,7 @@ const AnalogClock = () => {
   const secondAngle = (seconds / 60) * 360;
   const minutesAngle = (minutes / 60) * 360 + (seconds / 60) * 6;
   const hourAngle = ((hours % 12) / 12) * 360 + (minutes / 60) * 30;
-  const fullTime = MarqueeText(`(${weeknumber}) ${weekday} ${day}${suffix} ${months} ${year} ${hh}:${mm}:${ss} ${meridiem}`,250);
+  const fullTime = MarqueeText(`(${weeknumber}) ${weekday} ${day}${suffix} ${months} ${year} ${hh}:${mm}:${ss} ${meridiem}`,1000);
 
   useEffect(() => {
     const margin = 20;
